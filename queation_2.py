64e144637216b407f0e4ce0985ce2432bb7b3f43
prerna9960 <113686542+prerna9960@users.noreply.github.com>
2022-09-26 17:30:33 +0530

    Add files via upload

---
+++ queation_2.py
@@ -0,0 +1,10 @@
+
+def reverse_string(str):  
+    str1 = ""   # Declaring empty string to store the reversed string  
+    for i in str:  
+        str1 = i + str1  
+    return str1    # It will return the reverse string to the caller function  
+     
+str = "1234abcd"    # Given String       
+print("The original string is: ",str)  
+print("The reverse string is",reverse_string(str))
